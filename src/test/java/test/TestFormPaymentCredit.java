f4e180fc424cab509b27ae5e8a4fbdbb1c844635
Evgeniy Popov <evgenij.selin.94@bk.ru>
2023-08-10 17:46:44 +0300

    change all diplom commit

--- src/test/java/test/TestFormPaymentCredit.java
+++ src/test/java/test/TestFormPaymentCredit.java
@@ -1,6 +1,7 @@
 package test;
 
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.model.Status;
 import io.qameta.allure.selenide.AllureSelenide;
 import data.SQl;
 import page.FormPage;
@@ -38,9 +39,193 @@ public class TestFormPaymentCredit {
         formPage.setCardNumber("4444444444444441");
         formPage.setCardMonth("08");
         formPage.setCardYear("23");
-        formPage.setCardOwner("Evgeniy Selin");
+        formPage.setCardOwner("Ivan Popov");
         formPage.setCardCVV("999");
         formPage.pushСontinueButton();
         formPage.checkMessageSuccess();
+    }
+
+    @Test
+    @DisplayName("PaymentForAnInactiveCard")
+    void shouldNoPayByDeclinedCardInCredit() throws SQLException {
+        formPage.buyOnCredit();
+        formPage.setCardNumber("4444444444444442");
+        formPage.setCardMonth("08");
+        formPage.setCardYear("23");
+        formPage.setCardOwner("Ivan Popov");
+        formPage.setCardCVV("999");
+        formPage.pushСontinueButton();
+        formPage.checkMessageError();
+    }
+
+    @Test
+    @DisplayName("PaymentByUnknownCard")
+    void shouldNoPayByUnknownCardInCredit() throws SQLException {
+        formPage.buyOnCredit();
+        formPage.setCardNumber("4444444444444443");
+        formPage.setCardMonth("08");
+        formPage.setCardYear("23");
+        formPage.setCardOwner("Ivan Popov");
+        formPage.setCardCVV("999");
+        formPage.pushСontinueButton();
+        formPage.checkMessageError();
+    }
+
+    @Test
+    @DisplayName("PaymentByCardWithInvalidCardNumber")
+    void shouldNoPayInvalidCardNumberFieldInCredit() throws SQLException {
+        formPage.buyOnCredit();
+        formPage.setCardNumber("3333 2323 DSDF ASSD");
+        formPage.setCardMonth("08");
+        formPage.setCardYear("23");
+        formPage.setCardOwner("Ivan Popov");
+        formPage.setCardCVV("999");
+        formPage.pushСontinueButton();
+        formPage.checkMessageWrongFormat();
+    }
+
+    @Test
+    @DisplayName("PaymentByCardWithInvalidMonthNumber")
+    void shouldNoPayInvalidMonthFieldInCredit() throws SQLException {
+        formPage.buyOnCredit();
+        formPage.setCardNumber("4444444444444441");
+        formPage.setCardMonth("13");
+        formPage.setCardYear("23");
+        formPage.setCardOwner("Ivan Popov");
+        formPage.setCardCVV("999");
+        formPage.pushСontinueButton();
+        formPage.checkMessageWrongDate();
+    }
+
+    @Test
+    @DisplayName("PaymentByCardWithInvalidYearNumber")
+    void shouldNoPayInvalidYearFieldInCredit() throws SQLException {
+        formPage.buyOnCredit();
+        formPage.setCardNumber("4444444444444441");
+        formPage.setCardMonth("08");
+        formPage.setCardYear("18");
+        formPage.setCardOwner("Ivan Popov");
+        formPage.setCardCVV("999");
+        formPage.pushСontinueButton();
+        formPage.checkMessageOverDate();
+    }
+
+    @Test
+    @DisplayName("PaymentByCardWithInvalidOwnerField")
+    void shouldNoPayInvalidCardOwnerFieldInCredit() throws SQLException {
+        formPage.buyOnCredit();
+        formPage.setCardNumber("4444444444444441");
+        formPage.setCardMonth("08");
+        formPage.setCardYear("23");
+        formPage.setCardOwner("Bdfy 1213 Попов 12");
+        formPage.setCardCVV("999");
+        formPage.pushСontinueButton();
+        formPage.checkMessageError();
+    }
+
+    @Test
+    @DisplayName("PaymentByCardWithInvalidCVVField")
+    void shouldNoPayInvalidCVVFieldInCredit() throws SQLException {
+        formPage.buyOnCredit();
+        formPage.setCardNumber("4444444444444441");
+        formPage.setCardMonth("08");
+        formPage.setCardYear("23");
+        formPage.setCardOwner("Ivan Popov");
+        formPage.setCardCVV("12D");
+        formPage.pushСontinueButton();
+        formPage.checkMessageError();
+    }
+
+    @Test
+    @DisplayName("PaymentByCardWithAnEmptyCardNumber")
+    void shouldNoPayEmptyCardNumberFieldInCredit() throws SQLException {
+        formPage.buyOnCredit();
+        formPage.setCardNumber("");
+        formPage.setCardMonth("08");
+        formPage.setCardYear("23");
+        formPage.setCardOwner("Ivan Popov");
+        formPage.setCardCVV("999");
+        formPage.pushСontinueButton();
+        formPage.checkMessageWrongFormat();
+    }
+
+    @Test
+    @DisplayName("PaymentByCardWithAnEmptyMonthNumber")
+    void shouldNoPayEmptyMonthFieldInCredit() throws SQLException {
+        formPage.buyOnCredit();
+        formPage.setCardNumber("4444444444444441");
+        formPage.setCardMonth("");
+        formPage.setCardYear("23");
+        formPage.setCardOwner("Ivan Popov");
+        formPage.setCardCVV("999");
+        formPage.pushСontinueButton();
+        formPage.checkMessageWrongFormat();
+    }
+
+    @Test
+    @DisplayName("PaymentByCardWithAnEmptyYearNumber")
+    void shouldNoPayEmptyYearFieldInCredit() throws SQLException {
+        formPage.buyOnCredit();
+        formPage.setCardNumber("4444444444444441");
+        formPage.setCardMonth("08");
+        formPage.setCardYear("");
+        formPage.setCardOwner("Ivan Popov");
+        formPage.setCardCVV("999");
+        formPage.pushСontinueButton();
+        formPage.checkMessageWrongFormat();
+    }
+
+    @Test
+    @DisplayName("PaymentByCardWithAnEmptyFieldOwner")
+    void shouldNoPayEmptyCardOwnerFieldInCredit() throws SQLException {
+        formPage.buyOnCredit();
+        formPage.setCardNumber("4444444444444441");
+        formPage.setCardMonth("08");
+        formPage.setCardYear("23");
+        formPage.setCardOwner("");
+        formPage.setCardCVV("999");
+        formPage.pushСontinueButton();
+        formPage.checkMessageRequiredField();
+    }
+
+    @Test
+    @DisplayName("PaymentByCardWithAnEmptyCVVField")
+    void shouldNoPayEmptyCVVFieldInCredit() throws SQLException {
+        formPage.buyOnCredit();
+        formPage.setCardNumber("4444444444444441");
+        formPage.setCardMonth("08");
+        formPage.setCardYear("23");
+        formPage.setCardOwner("Ivan Popov");
+        formPage.setCardCVV("");
+        formPage.pushСontinueButton();
+        formPage.checkMessageWrongFormat();
+    }
+
+    @Test
+    @DisplayName("PaymentByActiveCardDatabaseRecordCheck")
+    void shouldPayByApprovedCardInCreditStatusInDB() throws SQLException {
+        formPage.buyOnCredit();
+        formPage.setCardNumber("4444444444444441");
+        formPage.setCardMonth("08");
+        formPage.setCardYear("23");
+        formPage.setCardOwner("Ivan Popov");
+        formPage.setCardCVV("999");
+        formPage.pushСontinueButton();
+        formPage.checkMessageSuccess();
+        SQl.checkCreditStatus(Status.PASSED);
+    }
+
+    @Test
+    @DisplayName("PaymentOnAnInactiveCardDatabaseRecordCheck")
+    void shouldPayByDeclinedCardInCreditStatusInDB() throws SQLException {
+        formPage.buyOnCredit();
+        formPage.setCardNumber("4444444444444442");
+        formPage.setCardMonth("08");
+        formPage.setCardYear("23");
+        formPage.setCardOwner("Ivan Popov");
+        formPage.setCardCVV("999");
+        formPage.pushСontinueButton();
+        formPage.checkMessageSuccess();
+        SQl.checkCreditStatus(Status.FAILED);
     }
 }
--- src/test/java/test/TestFormPaymentRegularCard.java
+++ src/test/java/test/TestFormPaymentRegularCard.java
@@ -1,6 +1,7 @@
 package test;
 
 import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.model.Status;
 import io.qameta.allure.selenide.AllureSelenide;
 import data.SQl;
 import page.FormPage;
@@ -43,6 +44,191 @@ TestFormPaymentRegularCard {
         formPage.setCardCVV("999");
         formPage.pushСontinueButton();
         formPage.checkMessageSuccess();
+    }
+
+    @Test
+    @DisplayName("PaymentForAnInactiveCard")
+    void shouldNoPayByDeclinedCard() throws SQLException {
+        formPage.buyForYourMoney();
+        formPage.setCardNumber("4444444444444442");
+        formPage.setCardMonth("08");
+        formPage.setCardYear("23");
+        formPage.setCardOwner("Ivan Popov");
+        formPage.setCardCVV("999");
+        formPage.pushСontinueButton();
+        formPage.checkMessageError();
+    }
+
+    @Test
+    @DisplayName("PaymentByUnknownCard")
+    void shouldNoPayByUnknownCard() throws SQLException {
+        formPage.buyForYourMoney();
+        formPage.setCardNumber("4444444444444443");
+        formPage.setCardMonth("08");
+        formPage.setCardYear("23");
+        formPage.setCardOwner("Ivan Popov");
+        formPage.setCardCVV("999");
+        formPage.pushСontinueButton();
+        formPage.checkMessageError();
+    }
+
+    @Test
+    @DisplayName("PaymentByCardWithInvalidCardNumber")
+    void shouldNoPayInvalidCardNumberField() throws SQLException {
+        formPage.buyForYourMoney();
+        formPage.setCardNumber("3333 2323 DSDF ASSD");
+        formPage.setCardMonth("08");
+        formPage.setCardYear("23");
+        formPage.setCardOwner("Ivan Popov");
+        formPage.setCardCVV("999");
+        formPage.pushСontinueButton();
+        formPage.checkMessageWrongFormat();
+    }
+
+    @Test
+    @DisplayName("PaymentByCardWithInvalidMonthNumber")
+    void shouldNoPayInvalidMonthField() throws SQLException {
+        formPage.buyForYourMoney();
+        formPage.setCardNumber("4444444444444441");
+        formPage.setCardMonth("13");
+        formPage.setCardYear("23");
+        formPage.setCardOwner("Ivan Popov");
+        formPage.setCardCVV("999");
+        formPage.pushСontinueButton();
+        formPage.checkMessageWrongDate();
+    }
+
+    @Test
+    @DisplayName("PaymentByCardWithInvalidYearNumber")
+    void shouldNoPayInvalidYearField() throws SQLException {
+        formPage.buyForYourMoney();
+        formPage.setCardNumber("4444444444444441");
+        formPage.setCardMonth("08");
+        formPage.setCardYear("18");
+        formPage.setCardOwner("Ivan Popov");
+        formPage.setCardCVV("999");
+        formPage.pushСontinueButton();
+        formPage.checkMessageOverDate();
+    }
+
+    @Test
+    @DisplayName("PaymentByCardWithInvalidOwnerField")
+    void shouldNoPayInvalidCardOwnerField() throws SQLException {
+        formPage.buyForYourMoney();
+        formPage.setCardNumber("4444444444444441");
+        formPage.setCardMonth("08");
+        formPage.setCardYear("23");
+        formPage.setCardOwner("Bdfy 1213 Попов 12");
+        formPage.setCardCVV("999");
+        formPage.pushСontinueButton();
+        formPage.checkMessageError();
+    }
+
+    @Test
+    @DisplayName("PaymentByCardWithInvalidCVVField")
+    void shouldNoPayInvalidCVVField() throws SQLException {
+        formPage.buyForYourMoney();
+        formPage.setCardNumber("4444444444444441");
+        formPage.setCardMonth("08");
+        formPage.setCardYear("23");
+        formPage.setCardOwner("Ivan Popov");
+        formPage.setCardCVV("12D");
+        formPage.pushСontinueButton();
+        formPage.checkMessageError();
+    }
+
+    @Test
+    @DisplayName("PaymentByCardWithAnEmptyCardNumber")
+    void shouldNoPayEmptyCardNumberField() throws SQLException {
+        formPage.buyForYourMoney();
+        formPage.setCardNumber("");
+        formPage.setCardMonth("08");
+        formPage.setCardYear("23");
+        formPage.setCardOwner("Ivan Popov");
+        formPage.setCardCVV("999");
+        formPage.pushСontinueButton();
+        formPage.checkMessageWrongFormat();
+    }
+
+    @Test
+    @DisplayName("PaymentByCardWithAnEmptyMonthNumber")
+    void shouldNoPayEmptyMonthField() throws SQLException {
+        formPage.buyForYourMoney();
+        formPage.setCardNumber("4444444444444441");
+        formPage.setCardMonth("");
+        formPage.setCardYear("23");
+        formPage.setCardOwner("Ivan Popov");
+        formPage.setCardCVV("999");
+        formPage.pushСontinueButton();
+        formPage.checkMessageWrongFormat();
+    }
+
+    @Test
+    @DisplayName("PaymentByCardWithAnEmptyYearNumber")
+    void shouldNoPayEmptyYearField() throws SQLException {
+        formPage.buyForYourMoney();
+        formPage.setCardNumber("4444444444444441");
+        formPage.setCardMonth("08");
+        formPage.setCardYear("");
+        formPage.setCardOwner("Ivan Popov");
+        formPage.setCardCVV("999");
+        formPage.pushСontinueButton();
+        formPage.checkMessageWrongFormat();
+    }
+
+    @Test
+    @DisplayName("PaymentByCardWithAnEmptyFieldOwner")
+    void shouldNoPayEmptyCardOwnerField() throws SQLException {
+        formPage.buyForYourMoney();
+        formPage.setCardNumber("4444444444444441");
+        formPage.setCardMonth("08");
+        formPage.setCardYear("23");
+        formPage.setCardOwner("");
+        formPage.setCardCVV("999");
+        formPage.pushСontinueButton();
+        formPage.checkMessageRequiredField();
+    }
+
+    @Test
+    @DisplayName("PaymentByCardWithAnEmptyCVVField")
+    void shouldNoPayEmptyCVVField() throws SQLException {
+        formPage.buyForYourMoney();
+        formPage.setCardNumber("4444444444444441");
+        formPage.setCardMonth("08");
+        formPage.setCardYear("23");
+        formPage.setCardOwner("Ivan Popov");
+        formPage.setCardCVV("");
+        formPage.pushСontinueButton();
+        formPage.checkMessageWrongFormat();
+    }
+
+
+    @Test
+    @DisplayName("PaymentByActiveCardDatabaseRecordCheck")
+    void shouldPayByApprovedCardStatusInDB() throws SQLException {
+        formPage.buyForYourMoney();
+        formPage.setCardNumber("4444444444444441");
+        formPage.setCardMonth("08");
+        formPage.setCardYear("23");
+        formPage.setCardOwner("Ivan Popov");
+        formPage.setCardCVV("999");
+        formPage.pushСontinueButton();
+        formPage.checkMessageSuccess();
+        SQl.checkPaymentStatus(Status.PASSED);
+    }
+
+    @Test
+    @DisplayName("PaymentOnAnInactiveCardDatabaseRecordCheck")
+    void shouldNoPayByDeclinedCardStatusInDB() throws SQLException {
+        formPage.buyForYourMoney();
+        formPage.setCardNumber("4444444444444442");
+        formPage.setCardMonth("08");
+        formPage.setCardYear("23");
+        formPage.setCardOwner("Ivan Popov");
+        formPage.setCardCVV("999");
+        formPage.pushСontinueButton();
+        formPage.checkMessageSuccess();
+        SQl.checkPaymentStatus(Status.FAILED);
     }
 }
 
